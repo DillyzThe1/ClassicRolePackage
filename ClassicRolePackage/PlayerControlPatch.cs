a8557f71a7228aebdae9ff903c745822349ca445
DillyzThe1 <dyl1029an@gmail.com>
2022-12-26 20:36:36 -0600

    require moving

--- ClassicRolePackage/PlayerControlPatch.cs
+++ ClassicRolePackage/PlayerControlPatch.cs
@@ -13,6 +13,7 @@ namespace ClassicRolePackage
     class PlayerControlPatch
     {
         public static DateTime lastTrace = DateTime.MinValue;
+        public static Dictionary<byte, Vector2> oldPos = new Dictionary<byte, Vector2>();
         public static void Postfix(PlayerControl __instance) {
             if (__instance != PlayerControl.LocalPlayer)
                 return;
@@ -27,12 +28,17 @@ namespace ClassicRolePackage
 
             foreach (PlayerControl player in PlayerControl.AllPlayerControls)
             {
+                Vector2 curpos = player.GetTruePosition();
+                if (player.inVent || (oldPos.ContainsKey(player.PlayerId) && oldPos[player.PlayerId].x == curpos.x && oldPos[player.PlayerId].y == curpos.y))
+                    return;
 
                 GameObject newStep = new GameObject();
                 newStep.layer = player.gameObject.layer;// - 1;
                 newStep.name = "footstep_" + newStep.name + "_" + lastTrace.ToString();
                 FootstepBehaviour footstep = newStep.AddComponent<FootstepBehaviour>();
                 footstep.BeginFunnies(player);
+
+                oldPos[player.PlayerId] = curpos;
             }
         }
     }
